8e4e307c16ca94b1f778462b904467ac70ed400f
nikhileshchenna <chennanikhilesh@gmail.com>
2025-07-20 14:22:21 +0530

    add : code

---
+++ task1.py
@@ -0,0 +1,14 @@
+# 🐍 Tiny Python Program: Greet & Add
+
+# Greet the user
+print("Hello there! 👋")
+
+# Ask for two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Add them
+sum = num1 + num2
+
+# Show the result
+print("The sum is:", sum)
